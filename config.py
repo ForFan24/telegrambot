f67bffb92b5153c7eb3abe14715815808562b8de
Margarita <podezzhikh95@bk.ru>
2023-03-27 18:02:02 +0300

    Перенесение валют и токена в файл config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
+TOKEN = '5734596114:AAFR5qMVJ6x2jG1RJBncW52IOw0k2yaAMP4'
--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import telebot
 import requests
 import json
+from config import *
 
-exchanges = {
-    'доллар': 'USD',
-    'евро': 'EUR',
-    'рубль': 'RUB'
-}
-TOKEN = '5734596114:AAFR5qMVJ6x2jG1RJBncW52IOw0k2yaAMP4'
 bot = telebot.TeleBot(TOKEN)
 
 
