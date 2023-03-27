741b907144aadab3f41f25f3754a4e6435ee3360
Margarita <podezzhikh95@bk.ru>
2023-03-27 17:59:33 +0300

    Добавление API

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import telebot
+import requests
+import json
 
 exchanges = {
     'доллар': 'USD',
@@ -23,6 +25,15 @@ def values(message: telebot.types.Message):
     for i in exchanges.keys():
         text = '\n'.join((text, i))
     bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text'])
+def converter(message: telebot.types.Message):
+    base, sym, amount = message.text.split()
+    r = requests.get(f"https://api.exchangeratesapi.io/latest?base={base}&symbols={sym}")
+    resp = json.loads(r.content)
+    new_price = resp['rates'][sym] * float(amount)
+    bot.reply_to(message, f"Цена {amount} {base} в {sym} : {new_price}")
 
 
 bot.polling()
