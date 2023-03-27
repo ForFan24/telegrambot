0acd3706c3a06d9880cd5e6757d761a02099799c
Margarita <podezzhikh95@bk.ru>
2023-03-27 17:31:15 +0300

    Добавление команд start,help, values

--- main.py
+++ main.py
@@ -7,3 +7,22 @@ exchanges = {
 }
 TOKEN = '5734596114:AAFR5qMVJ6x2jG1RJBncW52IOw0k2yaAMP4'
 bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите комманду боту в следующем формате:\n<имя валюты> \
+<в какую валюту перевести> \
+<количество переводимой валюты>\nУвидить список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for i in exchanges.keys():
+        text = '\n'.join((text, i))
+    bot.reply_to(message, text)
+
+
+bot.polling()
